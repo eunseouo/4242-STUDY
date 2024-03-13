fdb2fd02723e645386c2695b839ccf8796a8ec26
eunseo <shines3737@naver.com>
2024-03-13 23:46:33 +0900

    second commit

--- 4week/test/소정/maxmin.js
+++ 4week/test/소정/maxmin.js
@@ -1,20 +1,23 @@
 //값을 입력 받아 box 에 넣기
 var box1_value = prompt('첫 번째 값을 입력해주세요');
-document.getElementsByClassName('.box1').textContent = box1_value;
+console.log(box1_value);
+document.getElementsByClassName('.box1').textContent = box1_value.value;
 // div 박스에 안들어감.ㅡㅡ
 
 var box2_value = prompt('두 번째 값을 입력해주세요');
+console.log(box2_value);
 document.getElementsByClassName('.box2').textContent = box2_value;
 
 var box3_value = prompt('세 번째 값을 입력해주세요');
+console.log(box3_value);
 document.getElementsByClassName('.box3').textContent = box3_value;
 
 function Comparison() {
   //입력받은 값을 숫자로 변환함  parseint 안되면 number로 헤보셈
-  var box1_value = parseInt(document.querySelector('.box1').textContent);
-  var box2_value = parseInt(document.querySelector('.box2').textContent);
-  var box3_value = parseInt(document.querySelector('.box3').textContent);
-
+  var box1_value = Number(document.querySelector('.box1').textContent);
+  var box2_value = Number(document.querySelector('.box2').textContent);
+  var box3_value = Number(document.querySelector('.box3').textContent);
+  console.log(box1_value, box2_value, box3_value);
   var max, min;
 
   //if 문으로 대소비교
@@ -31,6 +34,11 @@ function Comparison() {
   } else if (box3_value < min) {
     min = box3_value;
   }
+  console.log(max);
+  console.log(min);
 
   var result = '가장 큰 수는 ' + max + ',가장 작은 수는 ' + min;
+
+  //결과 출력
+  document.querySelector('input[type="text"]').value = result;
 }
