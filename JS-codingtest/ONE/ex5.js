5916ec62e0353ed225fb54dd00558dfe7c32878f
은서 <162455166+eunseouo@users.noreply.github.com>
2024-10-04 23:57:06 +0900

    미완

---
+++ JS-codingtest/ONE/ex5.js
@@ -0,0 +1,17 @@
+// slice 함수 구현
+// main start!
+arr = [1, 2, 3, 4, 5, 6];
+start = 2;
+end = 3;
+solution(start, end);
+console.log(arr); // 배열이 수정된 결과 확인
+
+// main end!
+
+function solution(start, end) { //시작 인덱스, 제거할 요소 개수
+  removedArr = [];
+
+  return removedArr;
+}
+
+// 큰일이다.. 어렵다. 굉장한 난항.
